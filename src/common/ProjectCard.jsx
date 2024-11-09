d036f85e80ec45bf2827f2d9cb6dc950fc6b2f09
Manash Roy <manash.roy.dev@gmail.com>
2024-11-09 23:22:05 +0530

    updated the file

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { useTheme } from './ThemeContext';
 
-function ProjectCard({ src, link, h3, p }) {
+function ProjectCard({ src, link, h3, p, hostLink }) {
+
+  const { theme } = useTheme();
+  const style = { color: theme == 'light' ? "black" : "white" };
+
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
-      <h3>{h3}</h3>
+    <div>
+
+      <a href={link} target="_blank">
+        <img className="hover" src={src} alt={`${h3} logo`} />
+      </a>
+
+      <a href={hostLink} target="_blank">
+        <h3>{h3}</h3>
+        <FaExternalLinkAlt style={style} />
+      </a>
+
       <p>{p}</p>
-    </a>
+
+    </div>
   );
 }
 
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import Project3 from "../../assets/FindMyGit.png";
 import Project4 from "../../assets/BlogApp.png";
 
 function Projects() {
+
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
@@ -15,24 +16,28 @@ function Projects() {
           link="https://github.com/Man0sh-r0y/EduHub"
           h3="EduHub"
           p="EdTech Platform"
+          hostLink="https://eduhubbymanash.vercel.app"
         />
         <ProjectCard
           src={Project2}
           link="https://github.com/Man0sh-r0y/Shopping-Cart-using-Redux"
           h3="Shopping cart"
           p="E-commerce Cart Application"
+          hostLink="https://shopping-cart-by-manash.vercel.app"
         />
         <ProjectCard
           src={Project3}
           link="https://github.com/Man0sh-r0y/FindMyGit"
           h3="FindMyGit"
           p="Find Any Github User's data"
+          hostLink="https://find-github-users-by-manash.vercel.app"
         />
         <ProjectCard
           src={Project4}
           link="https://github.com/Man0sh-r0y/BlogVista"
           h3="BlogVista"
           p="Static Blogging App UI"
+          hostLink="https://blog-website-by-manash.vercel.app"
         />
       </div>
     </section>
