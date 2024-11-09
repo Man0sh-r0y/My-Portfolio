64548f280f46d97bec63e65c041717499d162cf0
Manash Roy <manash.roy.dev@gmail.com>
2024-11-09 21:05:34 +0530

    updated the file

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -3,35 +3,45 @@ import checkMarkIconDark from '../../assets/checkmark-dark.svg';
 import checkMarkIconLight from '../../assets/checkmark-light.svg';
 import SkillList from '../../common/SkillList';
 import { useTheme } from '../../common/ThemeContext';
+import HTML from "../../assets/HTML.png"
+import CSS from "../../assets/CSS.png"
+import JS from "../../assets/Javascript.svg"
+import Node from "../../assets/NodeJs.svg"
+import React from "../../assets/react.svg"
+import Tailwind from "../../assets/Tailwind.png"
+import Redux from "../../assets/Redux.svg"
+import Git from "../../assets/Git.svg"
+import MongoDB from "../../assets/MongoDB.svg"
+import Express from "../../assets/Express.svg"
+import Express2 from "../../assets/ExpressInDarkMode.svg"
+import SQL from "../../assets/SQL.svg"
+
 
 function Skills() {
   const { theme } = useTheme();
-  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+  const ExpressJsIcon = theme === 'light' ? Express : Express2;
 
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={HTML} skill="HTML" />
+        <SkillList src={CSS} skill="CSS" />
+        <SkillList src={JS} skill="JavaScript" />
+        <SkillList src={Node} skill="Node" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={React} skill="React" />
+        <SkillList src={Tailwind} skill="Tailwind CSS" />
+        <SkillList src={ExpressJsIcon} skill="Express" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={Redux} skill="Redux" />
+        <SkillList src={Git} skill="Git" />
+        <SkillList src={SQL} skill="SQL" />
+        <SkillList src={MongoDB} skill="MongoDB" />
       </div>
     </section>
   );
